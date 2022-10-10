e812801e256e905a8c32d14746bda38175ba514a
Alexander Fomin <firstmixlucker@gmail.com>
2022-10-10 12:18:49 +0300

    v0.1

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,23 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     mode: "development",
     watch: true,
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: ['./src/index.html', './src/styles/styles.scss', './src/index.js'],
     output: {
-      filename: '[name].[contenthash].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      assetModuleFilename: "[name][ext]",
+      publicPath: 'auto',
+      filename: '[name].[contenthash].js',
+      path: __dirname + '/dist',
       clean: true,
     },
     module: {
       rules: [
+        {
+          test: /\.html$/i,
+          loader: "html-loader",
+        },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
@@ -21,40 +26,48 @@ module.exports = {
             options: {
               presets: ['@babel/preset-env']
             }
-          }
+          },
         },
         {
           test: /\.(s(a|c)ss)$/,
           use: [
             'style-loader', // creates style nodes from JS strings
+            MiniCssExtractPlugin.loader,
             {
-              loader: 'css-loader', // translates CSS into CommonJS
+              loader: "css-loader",
               options: {
+                modules: true,
+                sourceMap: true,
                 importLoaders: 1
               }
             },
             'postcss-loader', // post process the compiled CSS
             'sass-loader', // compiles Sass to CSS, using Node Sass by default
-            // MiniCssExtractPlugin.loader,
-          ]
+          ],
+          type: 'javascript/auto'
         },
         {
-          test: /\.(woff|woff2|eot|ttf|svg|jpg|png)$/,
-          use: {
-            loader: 'url-loader',
-          },
-        }
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'static/images/[hash][ext][query]'
+          }
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: 'asset/resource',
+          generator: {
+            filename: 'static/fonts/[hash][ext][query]'
+          }
+        },
       ],
     },
     devServer: {
-      static: {
-        directory: path.join(__dirname, 'dist'),
-      },
       open: true,
       hot: true,
-      port: 3000,
+      port: 3001,
       watchFiles: {
-        paths: ['src/**/*.html'],
+        paths: ['src/**/*'],
       },
     },
     plugins: [
@@ -63,6 +76,8 @@ module.exports = {
         template: './src/index.html',
         filename: 'index.html',
       }),
-      new MiniCssExtractPlugin(),
+      new MiniCssExtractPlugin({
+        filename: "main.min.[hash].css"
+      }),
     ]
 };
